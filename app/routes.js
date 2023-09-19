23002767c226b0593641c31b3afd0fffc91c1796
David Evans <34478190+devansXD@users.noreply.github.com>
2023-09-19 08:44:54 +0100

    parent fsm journey

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,37 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Nationality
+router.post('/nationality-answer', function (req, res) {
+
+    // Make a variable 
+    var whatNationality = req.session.data['what-nationality']
+  
+    // Check whether the variable matches a condition
+    if (whatNationality == "british"){
+      // Send user to next page
+      res.redirect('/current/checker-parent/national-insurance')
+    } else {
+      // Send user to other page
+      res.redirect('/current/checker-parent/asylum-seeker')
+    }
+  
+  })
+
+  // Asylum number
+router.post('/asylum-answer', function (req, res) {
+
+    // Make a variable 
+    var seekingAsylum = req.session.data['seeking-asylum']
+  
+    // Check whether the variable matches a condition
+    if (seekingAsylum == "yes"){
+      // Send user to next page
+      res.redirect('/current/checker-parent/nass-number')
+    } else {
+      // Send user to other page
+      res.redirect('/current/checker-parent/check-answers')
+    }
+  
+  })
