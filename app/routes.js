701ec87f619cdb4dc354696b3cb6277bf31da99f
David Evans <34478190+devansXD@users.noreply.github.com>
2023-09-20 14:28:45 +0100

    parent fsm journey

--- app/routes.js
+++ app/routes.js
@@ -41,3 +41,38 @@ router.post('/asylum-answer', function (req, res) {
     }
   
   })
+
+  // Nationality / CHECK /
+router.post('/checker-nationality-answer', function (req, res) {
+
+  // Make a variable 
+  var whatNationality = req.session.data['what-nationality']
+
+  // Check whether the variable matches a condition
+  if (whatNationality == "british"){
+    // Send user to next page
+    res.redirect('/current/checker/national-insurance')
+  } else {
+    // Send user to other page
+    res.redirect('/current/checker/asylum-seeker')
+  }
+
+})
+
+
+  // Asylum number CHECKER
+  router.post('/checker-asylum-answer', function (req, res) {
+
+    // Make a variable 
+    var seekingAsylum = req.session.data['seeking-asylum']
+  
+    // Check whether the variable matches a condition
+    if (seekingAsylum == "yes"){
+      // Send user to next page
+      res.redirect('/current/checker/nass-number')
+    } else {
+      // Send user to other page
+      res.redirect('/current/checker/check-answers')
+    }
+  
+  })
