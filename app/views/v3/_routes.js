e7446f5513683f1446978067b0ef53ae4cb7b290
David Evans <34478190+devansXD@users.noreply.github.com>
2024-01-17 15:59:46 +0000

    Iterations for beta
     - iterated start now page
    - added links to mvp and post mvp
    - tidied up index page

---
+++ app/views/v3/_routes.js
@@ -0,0 +1,8 @@
+// Folder routes
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter('/current')
+
+require('./parent-soft-check/_routes')
+
+ 
+  
