3da08013a62c807cd1c71594159f34039fcda783
wailsey <Emma.WAILES@education.gov.uk>
2024-08-27 13:37:34 +0100

    Schools and LA folder reorganisation mvp3
    Refactor folders for MVP3 prototype - post MVP

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,18 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here - above the module.exports line
 
 
+//const _ = require('lodash')
+//const express = require('express')
+//const { fakerEN_GB: faker } = require('@faker-js/faker')
+//const moment = require('moment')
+//const path = require('path')
+//const router = express.Router()
+//const url = require('url')
+//const utils = require('./lib/utils')
+//const permissions = require('./filters/permissions.js').filters
+//const fs = require('fs')
+
+
 const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
