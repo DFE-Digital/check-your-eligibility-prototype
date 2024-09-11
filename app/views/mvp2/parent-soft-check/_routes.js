31c6aa48aa19f8d6591aecc2ac12b1d6f7f7f438
wailsey <Emma.WAILES@education.gov.uk>
2024-09-11 10:17:40 +0100

    Updating legacy links
    Updating legacy links
    
    Adding in end to appeal flow missing from school journey - noticed by Sam.

--- app/views/mvp2/parent-soft-check/_routes.js
+++ app/views/mvp2/parent-soft-check/_routes.js
@@ -1,7 +1,7 @@
 //const govukPrototypeKit = require(‘govuk-prototype-kit’)
 //const router = govukPrototypeKit.requests.setupRouter(‘/mvp2’)
 ///mvp2///
-//const routes = require(‘./views/mvp1/_routes’);
+//const routes = require(‘./views/mvp2/_routes’);
     // NI number CHECKER
     //router.post(‘/test-answer’, function (req, res) {
         // Make a variable
@@ -9,10 +9,10 @@
         // Check whether the variable matches a condition
        // if (nassNumber == “yes”){
           // Send user to next page
-       //   res.redirect(‘/mvp1/parent-soft-check/eligible’)
+       //   res.redirect(‘/mvp2/parent-soft-check/eligible’)
        // } else {
           // Send user to other page
-       //   res.redirect(‘/mvp1/parent-soft-check/nass-number’)
+       //   res.redirect(‘/mvp2/parent-soft-check/nass-number’)
       //  }
      // })
       //
