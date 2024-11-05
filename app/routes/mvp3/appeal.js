8a1e2f72ea374d4be0c139ca5cf27d7d302e1978
wailsey <Emma.WAILES@education.gov.uk>
2024-11-05 10:17:30 +0000

    Family Appeal flow
    Have evidence
    Upload evidence
    Guidance on upload

--- app/routes/mvp3/appeal.js
+++ app/routes/mvp3/appeal.js
@@ -1,2 +1,24 @@
 module.exports = function(router) {
+
+
+  ///have-evidence///
+
+  router.post('/mvp3-evidence', function (req, res) {
+    // Make a variable
+    var evidence = req.session.data['evidence']
+    // Check whether the variable matches a condition
+   if (evidence == "digital"){
+      // Send user to next page
+      res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance-digital')
+    } else if (evidence === "paper") {
+      // Send user to a different page for physical evidence
+      res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance');
+  } else {
+      // Skip evidence section
+      res.redirect('/mvp3/_family/account/appeal/add-child/child-details-blank');
+  }
+  })
+
+
+
 }
