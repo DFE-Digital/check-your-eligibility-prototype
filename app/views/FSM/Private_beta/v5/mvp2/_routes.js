4245d12999b722502f7f3e5e234e871626f80e41
wailsey <Emma.WAILES@education.gov.uk>
2025-02-10 09:53:59 +0000

    Index development ready updated - school user
    updates

---
+++ app/views/FSM/Private_beta/v5/mvp2/_routes.js
@@ -0,0 +1,11 @@
+// Folder routes
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter('/mvp2')
+
+require('./parent-soft-check/_routes')
+
+ 
+
+
+
+
