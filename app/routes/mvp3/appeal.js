b8b2c5fcd7888144f03faf96a93c413f25dad9a6
Andy Long <38563203+AndyLongContent@users.noreply.github.com>
2025-02-11 15:05:07 +0000

    Update v8 folder with design ideations

--- app/routes/mvp3/appeal.js
+++ app/routes/mvp3/appeal.js
@@ -1,22 +1,60 @@
 module.exports = function (router) {
 
-  ///have-evidence///
-  router.post('/mvp3-evidence', function (req, res) {
-    // Make a variable
-    var evidence = req.session.data['evidence']
-    // Check whether the variable matches a condition
-    if (evidence == "digital") {
-      // Send user to next page
-      res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance-digital')
-    } else if (evidence === "paper") {
-      // Send user to a different page for physical evidence
-      res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance');
-    } else {
-      // Skip evidence section
-      res.redirect('/mvp3/_family/account/appeal/add-child/child-details-blank');
-    }
-  })
-  ///////////////////////////////////////////////////////////////////////////////////////
+///have-evidence///
+router.post('/mvp3-evidence', function (req, res) {
+  // Make a variable
+  var evidence = req.session.data['evidence']
+  // Check whether the variable matches a condition
+  if (evidence == "digital") {
+    // Send user to next page
+    res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance-digital')
+  } else if (evidence === "paper") {
+    // Send user to a different page for physical evidence
+    res.redirect('/mvp3/_family/account/appeal/evidence/upload-guidance');
+  } else {
+    // Skip evidence section
+    res.redirect('/mvp3/_family/account/appeal/add-child/child-details-blank');
+  }
+})
+///////////////////////////////////////////////////////////////////////////////////////
+
+///have-evidence///
+router.post('/mvp3-school-evidence', function (req, res) {
+  // Make a variable
+  var evidence = req.session.data['schools_evidence']
+  // Check whether the variable matches a condition
+  if (evidence == "digital") {
+    // Send user to next page
+    res.redirect('/mvp3/_school/school-manage/upload/upload-guidance-digital');
+  } else if (evidence === "paper") {
+    // Send user to a different page for physical evidence
+    res.redirect('/mvp3/_school/school-manage/upload/upload-guidance');
+  } else if (evidence === "later") {
+    // Send user to a different page for uploading later
+    res.redirect('/mvp3/_school/school-manage/upload/appeal-check-answers-skip');
+  } else {
+    // Send by email
+    res.redirect('/mvp3/_school/school-manage/upload/appeal-check-answers-skip');
+  }
+
+})
+///////////////////////////////////////////////////////////////////////////////////////
+
+///have-evidence///
+router.post('/mvp3-school-add-evidence', function (req, res) {
+  // Make a variable
+  var evidence = req.session.data['schools_add_evidence']
+  // Check whether the variable matches a condition
+  if (evidence == "digital") {
+    // Send user to next page
+    res.redirect('/mvp3/_school//school-manage/upload/upload-guidance-digital-add')
+  } else if (evidence === "paper") {
+    // Send user to a different page for physical evidence
+    res.redirect('/mvp3/_school/school-manage/upload/upload-guidance-add');
+  }
+
+})
+///////////////////////////////////////////////////////////////////////////////////////
 
   ///temp gov routes///
   router.post('/mvp3-gov', function (req, res) {
