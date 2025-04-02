0ee49d53da0d629bd6f2a7ec0436878c5880f138
wailsey <Emma.WAILES@education.gov.uk>
2025-04-02 17:04:12 +0100

    LA batch check updates
    Versioning of batch check for LAs

---
+++ app/assets/javascripts/child-details.js
@@ -0,0 +1,80 @@
+accessibleAutocomplete.enhanceSelectElement({
+    defaultValue: '',
+    selectElement: document.querySelector('#school-picker')
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var container = document.querySelector('[data-module="moj-add-another"]');
+
+    // Event delegation for add and remove buttons
+    container.addEventListener('click', function (event) {
+        if (event.target.classList.contains('moj-add-another__add-button')) {
+            event.preventDefault(); // Prevent default action if needed
+            addChild(); // Call function to add a new child
+        }
+        if (event.target.classList.contains('moj-add-another__remove-button')) {
+            event.preventDefault(); // Prevent default action if needed
+            removeChild(event.target); // Call function to remove a child
+        }
+    });
+
+    function addChild() {
+        // Logic to add a new child input section
+        const currentCount = document.querySelectorAll('.moj-add-another__title').length;
+        const newChildHTML = `
+            <fieldset class="moj-add-another__item">
+                <legend class="moj-add-another__title govuk-fieldset__legend--m">Child ${currentCount + 1}</legend>
+                <div>
+                    <label for="person[${currentCount}][first_name]" class="govuk-label">First name</label>
+                    <input type="text" name="person[${currentCount}][first_name]" class="govuk-input" data-name="person[${currentCount}][first_name]" data-id="person[${currentCount}][first_name]"/>
+                </div>
+                <div>
+                    <label for="person[${currentCount}][last_name]" class="govuk-label">Last name</label>
+                    <input type="text" name="person[${currentCount}][last_name]" class="govuk-input" data-name="person[${currentCount}][last_name]" data-id="person[${currentCount}][last_name]"/>
+                </div>
+                <div>
+                    <label class="govuk-label">Date of birth</label>
+                    <div>
+                        <input type="text" name="person[${currentCount}][dob][day]" class="govuk-input" data-name="person[${currentCount}][dob][day]" placeholder="Day"/>
+                        <input type="text" name="person[${currentCount}][dob][month]" class="govuk-input" data-name="person[${currentCount}][dob][month]" placeholder="Month"/>
+                        <input type="text" name="person[${currentCount}][dob][year]" class="govuk-input" data-name="person[${currentCount}][dob][year]" placeholder="Year"/>
+                    </div>
+                </div>
+                <button type="button" class="govuk-button moj-add-another__remove-button">Remove this child</button>
+            </fieldset>
+        `;
+        container.insertAdjacentHTML('beforeend', newChildHTML);
+        updateLegends(); // Update legends after adding a new child
+    }
+
+    function removeChild(button) {
+        // Logic to remove the child input section
+        const fieldset = button.closest('fieldset');
+        if (fieldset) {
+            fieldset.remove();
+            updateLegends(); // Update legends after removing a child
+        }
+    }
+
+    function updateLegends() {
+        const legends = document.querySelectorAll('.moj-add-another__title');
+        legends.forEach(function (legend, index) {
+            legend.innerHTML = `Child ${index + 1}`; // Update legend to reflect current count
+        });
+    }
+});
+</script>
+<div class="govuk-grid-row">
+    <div class="govuk-grid-column-one-half">
+        <div data-module="moj-add-another">
+            <h1 class="govuk-heading-l" tabindex="-1">Provide details of your children</h1>
+            <form method="post">
+                {% call govukFieldset({
+                    classes: 'moj-add-another__item'
+                }) %}
+            </form>
+                <!-- Server-side templating syntax should remain outside JavaScript -->
+                {% call govukFieldset({
+                    classes: 'moj-add-another__item'
+                }) %}
+{% endblock %}
--- app/views/to_remove/X_routes.js
+++
@@ -1,7 +0,0 @@
-// Folder routes
-//const govukPrototypeKit = require('govuk-prototype-kit')
-//const router = govukPrototypeKit.requests.setupRouter('/postmvp_v1')
-
-//require('./_family/parent-soft-check/_routes')
-
-
