5ccc56c51c47533f884091855658c6cd11892e6a
wailsey <Emma.WAILES@education.gov.uk>
2025-07-06 19:10:03 +0100

    Clean up and additional pages for request more evidence journey
    LA journeys
    School journeys
    MAT dashboard
    Index updated

---
+++ app/routes/fsm/private_beta/v7-3/appeal-decision.js
@@ -0,0 +1,24 @@
+module.exports = function (router) {
+
+router.get('/fsm/private_beta/v7-3/test-johnson1', function (req, res) {
+  res.send('Route works!');
+});
+
+
+router.post('/fsm/private_beta/v7-3/appeal-decision/johnson1', function (req, res) {
+  const decision = req.session.data['decision'];
+
+  if (decision === 'approve') {
+    res.redirect('/fsm/private_beta/v7-3/LA/la-manage/decision/records/approve-johnson1');
+  } else if (decision === 'decline') {
+    res.redirect('/fsm/private_beta/v7-3/LA/la-manage/decision/records/decline-johnson1');
+  } else if (decision === 'pending') {
+    res.redirect('/fsm/private_beta/v7-3/LA/la-manage/decision/records/requestmore-johnson1');
+  } else {
+    res.redirect('/fsm/private_beta/v7-3/appeal-decision');
+  }
+});
+
+
+
+};
