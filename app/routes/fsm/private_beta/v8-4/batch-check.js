f4ff334a4bbf3e4b51384a9ff4e5f511fd266b4e
wailsey <Emma.WAILES@education.gov.uk>
2026-09-24 14:24:38 +0100

    Version 8-4 added for transfer pupils
    Added new version in to start prototyping transfer pupils

---
+++ app/routes/fsm/private_beta/v8-4/batch-check.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/v7-1-batch-check', function (req, res) {
+    console.log('Route hit');
+    const batchCheckStatus = req.session.data['batch-check'];
+    console.log('Batch Check Status:', batchCheckStatus);
+    if (batchCheckStatus === "connect") {
+        res.redirect('/FSM/Private_beta/v7-1/school/school-manage/batch-checking/school-mis/select-mis.html');
+    } else if (batchCheckStatus === "manual") {
+        res.redirect('/FSM/Private_beta/v7-1/school/school-manage/batch-checking/manual.html');
+    } else {
+        res.redirect('/FSM/Private_beta/v7-1/school/school-manage/batch-checking/manual-error.html');
+    }
+});
+
+module.exports = router;
+
